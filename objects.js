0d014101fb95d63b6597d6c6ce63f4c2b3ad0c4e
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-09 04:03:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {Hozier: "Work Song"};
 function updatePlaylist(playlist,artistName,songTitle) {
+<<<<<<< HEAD
   playlist[artistName] = songTitle
   return playlist
 }
@@ -7,3 +8,8 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
 }
+=======
+  playlist.artistName = songTitle
+  return playlist
+}
+>>>>>>> ac1a2840e779ff90bcd4284ad3ca87a3294ce879
